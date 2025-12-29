bcbf707e5f378a66f83f4246672d8d4733d07eab
Jess Virgil Lanchinebre <jevi.lanchinebre@gmail.com>
2025-12-29 11:10:46 -0800

    add initial template files

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import LandingPage from './components/LandingPage.jsx'
+
+const App = () => {
+  return (<LandingPage/>)
+}
+
+export default App
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,52 @@
+import React, { useRef } from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
+const style = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  pointerEvents: 'none',
+  backgroundColor: '#F5F5DC'
+}
+
+
+const url = (name, wrap = false) =>
+  `${
+    wrap ? 'url(' : ''
+  }./src/assets/${name}.png${
+    wrap ? ')' : ''
+  }`
+
+export default function App() {
+  const parallax = useRef(null)
+  return (
+    <div>
+      <Parallax ref={parallax} pages={3}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          style={style}
+        >
+          <div style={{width: '80%', margin: 'auto'}}>
+            <img src={url('OALogo')} style={{ width: '25%', display: 'block'}} />
+            <div>
+              <h1>welcome to design services!</h1>
+            </div>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={1}
+          speed={1}
+          style={style}>
+          <h1>what we offer?</h1>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          style={style}>
+            <h1>meet the team.</h1>
+          </ParallaxLayer>
+      </Parallax>
+    </div>
+  )
+}
